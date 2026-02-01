84a95571ad2f4d46cc95f5275006b536bf41b6f1
Tanveer <kntanveer7@gmail.com>
2026-02-01 19:27:33 +0530

    generate order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [];
+export let cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+}];
 
 export function addToCart(productId) {
   const quantityValue = Number(document.querySelector('.product-quantity-selector-' + productId).value);
